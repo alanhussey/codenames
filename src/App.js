554c25955f49c0619f8c4eda7ff3b1047106089c
Alan Hussey <alan.hussey@gmail.com>
2020-05-28 20:32:44 -0700

    render a constant board

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const RED = "r";
+const BLU = "b";
+const EXX = "X";
+const BLN = " ";
+
+const card = [
+  [BLN, RED, RED, BLU, RED],
+  [BLN, BLU, BLN, BLN, BLU],
+  [EXX, BLU, BLN, RED, BLN],
+  [RED, RED, BLU, BLU, RED],
+  [BLU, BLU, BLU, BLN, RED],
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+          {card.map((row, index) => (
+            <tr key={index}>
+              {row.map((cell, index) => (
+                <td
+                  key={index}
+                  style={{
+                    backgroundColor: {
+                      [RED]: "red",
+                      [BLU]: "blue",
+                      [EXX]: "black",
+                      [BLN]: "antiquewhite",
+                    }[cell],
+                  }}
+                >
+                  {
+                    {
+                      [RED]: "◇",
+                      [BLU]: "⚪",
+                      [EXX]: "×",
+                      [BLN]: " ",
+                    }[cell]
+                  }
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
